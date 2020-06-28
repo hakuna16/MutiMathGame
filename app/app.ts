a13e9f9d50a3c6a62136445aca5936fe1a5d913f
Rituj Kumar <rituj.kumar@tesco.com>
2020-06-29 00:54:04 +0530

    adidng fumnctions and function types

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,43 @@
 function startGame() {
-  let playerName = "Rituj";
+  let playerName: string | undefined = getInputValue('playername');
   logPlayer(playerName);
   // starting a new game
-  var messagesElement = document.getElementById("messages");
-  messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+
+  postScore(100, playerName)
+
+  postScore(-100, playerName)
 }
 
-function logPlayer(name: string) {
+const logPlayer = (name: string = "Multi Map Player") =>
   console.log(`Player name is ${name}`);
+
+
+const getInputValue = (id: string): string | undefined => {
+  const element: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(id)
+  );
+  return element.value === "" ? undefined : element.value;
 }
+
+function postScore(score: number, playerName: String = "Multi Map Player"): void {
+
+  let logger: (value: string) => void;
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
+
+  const scoreElement: HTMLElement = <HTMLElement>document.getElementById('postedScores')
+  scoreElement!.innerText = `${score}.....${playerName}`;
+
+  logger(`Score: ${score}`)
+}
+
 document.getElementById("startGame")!.addEventListener("click", startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string) {
+  console.error(err);
+}
